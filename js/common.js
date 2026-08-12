13ef89405337d965c0750366f82d4c3f39ec57a6
Avinash Malhotra <avimalhotra505@gmail.com>
2026-08-12 12:03:43 +0530

    contact page added

---
+++ js/common.js
@@ -0,0 +1,29 @@
+"use strict";
+$(function(){
+     
+     $(".menu").click(function(){
+          $("nav").slideToggle();
+          $(this).toggleClass("active");
+     });
+
+     $(".wrap-2 .vert").click(function(e){
+          e.preventDefault();
+
+          $(".wrap-2 .vert").removeClass("active");
+          $(this).addClass("active");
+
+          $(".wrap-2 .collapse").fadeOut(0);
+          $(this).next().fadeIn();
+
+     });
+
+     $(".slider").slick({
+          autoplay:true,
+          arrows:false,
+          
+     })
+
+
+
+
+});
